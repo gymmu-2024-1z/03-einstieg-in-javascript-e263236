89d0cdf8b2d9b1169e130e263fca3bac36915ecd
e263236 <e263236@edu.sbl.ch>
2024-10-16 14:38:35 +0000

    aufgabe 1 und 2

--- script.js
+++ script.js
@@ -29,8 +29,15 @@ export function aufgabe01(args) {
     // damit wir nicht so viele Klammern schreiben müssen.
     const currentElement = input[i]
 
-    // Hier wird das aktuelle Zeichen ans Ende der Resultat-Liste angehängt.
-    result.push(currentElement)
+    if (currentElement === "e") {
+      // do nothing
+    } else if (currentElement === "E") {
+      // auch E ignorieren
+    } else {
+      result.push(currentElement)
+    }
+
+    // Hier wird das aktuelle Zeichen ans Ende der Resultat-Liste angehängt
   }
 
   // Hier geben wir das Resultat zurück, und machen einen Text daraus.
@@ -38,3 +45,34 @@ export function aufgabe01(args) {
 }
 
 linkupExerciseHandler("[data-click=aufgabe01]", aufgabe01)
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  //Läuft zeichen für Zeichen über den ganzen Text
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    const upperCase = currentElement.toUpperCase()
+
+    //Alle Zeichen werden doppelt aufgeschrieben
+    result.push(upperCase)
+  }
+
+  return result.join("")
+}
+linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    count = count + 1
+  }
+  return count
+}
+linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
